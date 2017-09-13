aeddec79b8fcd355b6b8b9a423044b2178066469
Mark Tonks <marktonks75@gmail.com>
2017-09-13 20:01:55 +0100

    added screen level validation for edit/delete account in setup screen

--- public/js/setup/accounts.js
+++ public/js/setup/accounts.js
@@ -15,6 +15,12 @@ $(function() {
         $("#lblMandatoryFields").hide();
     });
 
+    $("#editModal").on("hidden.bs.modal", function() {
+      $(".mandatory").removeClass("form-inline-error-bgcolor");
+      $(".mandatory-delete").removeClass("form-inline-error-bgcolor");
+      $("#frmEdit #lblMandatoryFields").hide();
+    })
+
     //when edit button is clicked
     $(".btn-edit-ag").on("click", function(event) {
         $("#inputEditAcctCode").val($(this).data('accountcode'));
@@ -29,13 +35,8 @@ $(function() {
       $(".mandatory").removeClass("form-inline-error-bgcolor");
       $("#warning-labels label").hide();
 
-      let emptyInput = false;
-      $("#frmCreateAcct .mandatory").each(function(val, idx) {
-          if ($(this).val() === "") {
-            emptyInput = true;
-          }
-      });
-      if (emptyInput) {
+      if (checkForMandatoryFields($("#frmCreateAcct .mandatory"))) {
+        $("input, select").removeClass("form-inline-error-bgcolor");
         $("#lblMandatoryFields").show();
         $(".mandatory").addClass("form-inline-error-bgcolor");
         $("input:visible:first").focus();
@@ -50,21 +51,48 @@ $(function() {
 
     //when save button clicked on edit account modal
     $("#btnEditSaveAcct").on("click", function() {
+      if (checkForMandatoryFields($("#frmEdit .mandatory"))) {
+        $("input, select").removeClass("form-inline-error-bgcolor");
+        $("#frmEdit #lblMandatoryFields").show();
+        $(".mandatory").addClass("form-inline-error-bgcolor");
+        $(".mandatory:visible:first").focus();
+      } else {
         $("#editModal").modal("hide");
         $("#frmEdit #inputAction").val("update");
         $("#frmEdit").submit();
+      }
     });
 
     //when delete button clicked on edit account modal
     $("#btnEditDeleteAcct").on("click", function() {
-        $("#editModal").modal("hide");
-        showConfirmDialog("Delete Account -- Confirm?",
-                          "Are you sure you want to permanently delete this account? " +
-                          "This process cannot be reversed!",
-                          function() {
-           $("#frmEdit #inputAction").val("delete");
-           $("#frmEdit").submit();
-         });
+        if (checkForMandatoryFields($("#frmEdit .mandatory-delete"))) {
+          $("input, select").removeClass("form-inline-error-bgcolor");
+          $("#frmEdit #lblMandatoryFields").show();
+          $(".mandatory-delete").addClass("form-inline-error-bgcolor");
+          $(".mandatory-delete:visible:first").focus();
+          return false;
+        } else {
+          $("#editModal").modal("hide");
+          showConfirmDialog("Delete Account -- Confirm?",
+          "Are you sure you want to permanently delete this account? " +
+          "This process cannot be reversed!",
+          function() {
+            $("#frmEdit #inputAction").val("delete");
+            $("#frmEdit").submit();
+          });
+        };
+
     });
 
 })
+
+
+function checkForMandatoryFields(fldList) {
+  let emptyInput = false;
+  fldList.each(function(val, idx) {
+      if ($(this).val() === "") {
+        emptyInput = true;
+      }
+  });
+  return emptyInput;
+}
